4585a3e356e5e6bc28063756082db3318f90ccfe
Rijin Johnson <rijinpunthala007@gmail.com>
2025-12-03 20:40:31 +0530

    chore: configure vite for network access

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This exposes the app to your local network
+  }
 })
